4147596b52e2075b09d5a4fd4b69d8f78834311c
nizienko <nizienko@yamoney.ru>
2018-06-26 13:55:22 +0300

    test pr

--- src/main/kotlin/ru/yamoney/test/app/App.kt
+++ src/main/kotlin/ru/yamoney/test/app/App.kt
@@ -1,15 +1,12 @@
 package ru.yamoney.test.app
 
 import io.ktor.application.call
+import io.ktor.request.uri
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
-import org.litote.kmongo.eq
-import org.litote.kmongo.findOne
-import org.litote.kmongo.getCollection
-import ru.yamoney.test.app.db.db
 
 data class TestEntity(val name: String, val surname: String)
 
@@ -18,7 +15,9 @@ fun main(args: Array<String>) {
         routing {
             get("/") {
                 try {
-                    println(call.request.toString())
+                    println(call.request.uri)
+                    println(call.request.queryParameters)
+                    call.respondText { "Hello" }
                 } catch (e: Throwable) {
                     e.printStackTrace()
                 }
