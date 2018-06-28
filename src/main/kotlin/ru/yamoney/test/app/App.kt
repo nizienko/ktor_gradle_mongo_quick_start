59d99cd1d3f4ffe69973e9869aa41857caaeded6
nizienko <nizienko@yamoney.ru>
2018-06-28 14:36:46 +0300

    test pr

--- src/main/kotlin/ru/yamoney/test/app/App.kt
+++ src/main/kotlin/ru/yamoney/test/app/App.kt
@@ -2,8 +2,13 @@ package ru.yamoney.test.app
 
 import io.ktor.application.ApplicationCallPipeline
 import io.ktor.application.call
+import io.ktor.features.toLogString
+import io.ktor.http.HttpStatusCode
+import io.ktor.request.ApplicationRequest
+import io.ktor.request.contentType
+import io.ktor.request.httpMethod
 import io.ktor.request.uri
-import io.ktor.response.respondText
+import io.ktor.response.respond
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import io.ktor.util.toMap
@@ -17,11 +22,18 @@ fun main(args: Array<String>) {
                 println(call.request.uri)
                 println(call.request.queryParameters.toMap())
                 call.request.headers.forEach { s, list -> println("$s: $list") }
-
-                call.respondText { "Hello" }
+                processRequest(call.request)
+                call.respond(HttpStatusCode.OK)
             } catch (e: Throwable) {
                 e.printStackTrace()
+                call.respond(HttpStatusCode.BadRequest, e.message ?: e.localizedMessage)
             }
         }
     }.start(wait = true)
+}///asdasdasdasd
+
+fun processRequest(request: ApplicationRequest) {
+    println(request.httpMethod)
+    println(request.contentType())
+    println(request.toLogString())
 }
