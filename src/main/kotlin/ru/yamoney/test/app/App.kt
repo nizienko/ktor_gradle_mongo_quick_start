2cfc337be48945e7d305de740762c67499b67eb3
nizienko <nizienko@yamoney.ru>
2018-06-28 14:51:06 +0300

    test pr

--- src/main/kotlin/ru/yamoney/test/app/App.kt
+++ src/main/kotlin/ru/yamoney/test/app/App.kt
@@ -1,32 +1,46 @@
 package ru.yamoney.test.app
 
-import io.ktor.application.ApplicationCallPipeline
 import io.ktor.application.call
-import io.ktor.features.toLogString
-import io.ktor.http.HttpStatusCode
+import io.ktor.application.install
+import io.ktor.features.*
+import io.ktor.gson.gson
 import io.ktor.request.ApplicationRequest
 import io.ktor.request.contentType
 import io.ktor.request.httpMethod
-import io.ktor.request.uri
-import io.ktor.response.respond
+import io.ktor.routing.post
+import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
-import io.ktor.util.toMap
+import java.text.DateFormat
 
 data class TestEntity(val name: String, val surname: String)
 
 fun main(args: Array<String>) {
     embeddedServer(Netty, port = 8080) {
-        intercept(ApplicationCallPipeline.Call) {
-            try {
-                println(call.request.uri)
-                println(call.request.queryParameters.toMap())
-                call.request.headers.forEach { s, list -> println("$s: $list") }
-                processRequest(call.request)
-                call.respond(HttpStatusCode.OK)
-            } catch (e: Throwable) {
-                e.printStackTrace()
-                call.respond(HttpStatusCode.BadRequest, e.message ?: e.localizedMessage)
+        /*        intercept(ApplicationCallPipeline.Call) {
+                    try {
+                        println(call.request.uri)
+                        println(call.request.queryParameters.toMap())
+                        call.request.headers.forEach { s, list -> println("$s: $list") }
+                        processRequest(call.request)
+                        call.respond(HttpStatusCode.OK)
+                    } catch (e: Throwable) {
+                        e.printStackTrace()
+                        call.respond(HttpStatusCode.BadRequest, e.message ?: e.localizedMessage)
+                    }
+                }*/
+        install(DefaultHeaders)
+        install(Compression)
+        install(CallLogging)
+        install(ContentNegotiation) {
+            gson {
+                setDateFormat(DateFormat.LONG)
+                setPrettyPrinting()
+            }
+        }
+        routing {
+            post("/hook/{repository}") {
+                println(call.parameters["repository"])
             }
         }
     }.start(wait = true)
