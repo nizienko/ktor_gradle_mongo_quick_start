d7182c527d48cfc48bf4dc46233c331ab8f1b785
nizienko <nizienko@yamoney.ru>
2018-06-28 13:05:12 +0300

    test pr

--- src/main/kotlin/ru/yamoney/test/app/App.kt
+++ src/main/kotlin/ru/yamoney/test/app/App.kt
@@ -4,7 +4,6 @@ import io.ktor.application.ApplicationCallPipeline
 import io.ktor.application.call
 import io.ktor.request.uri
 import io.ktor.response.respondText
-import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import io.ktor.util.toMap
@@ -13,15 +12,13 @@ data class TestEntity(val name: String, val surname: String)
 
 fun main(args: Array<String>) {
     embeddedServer(Netty, port = 8080) {
-        routing {
-            intercept(ApplicationCallPipeline.Call) {
-                try {
-                    println(call.request.uri)
-                    println(call.request.queryParameters.toMap())
-                    call.respondText { "Hello" }
-                } catch (e: Throwable) {
-                    e.printStackTrace()
-                }
+        intercept(ApplicationCallPipeline.Call) {
+            try {
+                println(call.request.uri)
+                println(call.request.queryParameters.toMap())
+                call.respondText { "Hello" }
+            } catch (e: Throwable) {
+                e.printStackTrace()
             }
         }
     }.start(wait = true)
