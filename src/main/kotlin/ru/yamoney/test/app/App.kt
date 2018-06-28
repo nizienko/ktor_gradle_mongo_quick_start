cbf63752454162194256615212d9002b504c2602
nizienko <nizienko@yamoney.ru>
2018-06-28 12:53:56 +0300

    test pr

--- src/main/kotlin/ru/yamoney/test/app/App.kt
+++ src/main/kotlin/ru/yamoney/test/app/App.kt
@@ -1,9 +1,9 @@
 package ru.yamoney.test.app
 
+import io.ktor.application.ApplicationCallPipeline
 import io.ktor.application.call
 import io.ktor.request.uri
 import io.ktor.response.respondText
-import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
@@ -14,7 +14,7 @@ data class TestEntity(val name: String, val surname: String)
 fun main(args: Array<String>) {
     embeddedServer(Netty, port = 8080) {
         routing {
-            get("/") {
+            intercept(ApplicationCallPipeline.Call) {
                 try {
                     println(call.request.uri)
                     println(call.request.queryParameters.toMap())
